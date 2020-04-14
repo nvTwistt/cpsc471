cc1a220aec4f82542d9a92f9695eb468a8611285
MannyRMA <59375013+MannyRMA@users.noreply.github.com>
2020-04-13 22:23:52 -0600

    Updated app so it had Entities from our project

--- app.py
+++ app.py
@@ -1,102 +1,160 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy 
-from flask_marshmallow import Marshmallow 
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
-
 
 # Init app
 app = Flask(__name__)
+
+# base directory
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init db
+
+# Init the Database
 db = SQLAlchemy(app)
-# Init ma
-ma = Marshmallow(app)
 
-# Product Class/Model
-class Product(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100), unique=True)
-  description = db.Column(db.String(200))
-  price = db.Column(db.Float)
-  qty = db.Column(db.Integer)
+# Init Marshmallow
+marsh = Marshmallow(app)
 
-  def __init__(self, name, description, price, qty):
-    self.name = name
-    self.description = description
-    self.price = price
-    self.qty = qty
+################################################################### INVESTOR CLASS/ENTITY #####################################################################################
+class Investor(db.Model):
+    investorId = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    dateOfBirth = db.Column(db.String(40))
 
-# Product Schema
-class ProductSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'name', 'description', 'price', 'qty')
+    def __init__(self, name, dob):
+        self.name = name
+        self.dateOfBirth = dob
 
-# Init schema
-#
-#product_schema = ProductSchema(strict=True)
-#products_schema = ProductSchema(many=True, strict=True)
+# Investor Schema
+class InvestorSchema(marsh.Schema):
+    class Meta:
+        fields = ('investorId', 'name', 'dateOfBirth')
 
-# Create a Product
-@app.route('/product', methods=['POST'])
-def add_product():
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
+# Init the schema
+investor_schema = InvestorSchema()    # dealing with a single investor
+investors_schema = InvestorSchema(many=True)  # dealing with many investors
 
-  new_product = Product(name, description, price, qty)
+# Create an investor
+@app.route('/investor', methods=['POST'])
+def addInvestor():
+    name = request.json['name']
+    dateOfBirth = request.json['dateOfBirth']
 
-  db.session.add(new_product)
-  db.session.commit()
+    newInvestor = Investor(name, dateOfBirth)
 
-  return product_schema.jsonify(new_product)
+    db.session.add(newInvestor)
+    db.session.commit()
 
-# Get All Products
-@app.route('/product', methods=['GET'])
-def get_products():
-  all_products = Product.query.all()
-  result = products_schema.dump(all_products)
-  return jsonify(result.data)
+    return investor_schema.jsonify(newInvestor)
 
-# Get Single Products
-@app.route('/product/<id>', methods=['GET'])
-def get_product(id):
-  product = Product.query.get(id)
-  return product_schema.jsonify(product)
+# Get single Investor
+@app.route('/investor/<investorId>', methods=['GET'])
+def getInvestor(investorId):
+    investor = Investor.query.get(investorId)
+    return investor_schema.jsonify(investor)
 
-# Update a Product
-@app.route('/product/<id>', methods=['PUT'])
-def update_product(id):
-  product = Product.query.get(id)
+# Get All Investors
+@app.route('/investor', methods=['GET'])
+def getAllInvestors():
+    allInvestors = Investor.query.all()
+    result = investors_schema.dump(allInvestors)
+    return jsonify(result)
 
-  name = request.json['name']
-  description = request.json['description']
-  price = request.json['price']
-  qty = request.json['qty']
+# Update an Investor
+@app.route('/investor/<investorId>', methods=['PUT'])
+def updateInvestor(investorId):
+    investor = Investor.query.get(investorId)
 
-  product.name = name
-  product.description = description
-  product.price = price
-  product.qty = qty
+    name = request.json['name']
+    dateOfBirth = request.json['dateOfBirth']
 
-  db.session.commit()
+    investor.name = name
+    investor.dateOfBirth = dateOfBirth
 
-  return product_schema.jsonify(product)
+    db.session.commit()
 
-# Delete Product
-@app.route('/product/<id>', methods=['DELETE'])
-def delete_product(id):
-  product = Product.query.get(id)
-  db.session.delete(product)
-  db.session.commit()
+    return investor_schema.jsonify(investor)
 
-  return product_schema.jsonify(product)
+# Delete Investor
+@app.route('/investor/<investorId>', methods=['DELETE'])
+def deleteInvestor(investorId):
+    investor = Investor.query.get(investorId)
+    db.session.delete(investor)
+    db.session.commit()
+    return investor_schema.jsonify(investor)
 
+############################################################# REPORT CLASS/ENTITY ####################################################################################################
+class Company(db.Model):
+    companyName = db.Column(db.String(50), primary_key=True)
+    industry = db.Column(db.String(30))
+    sharesOutstanding = db.Column(db.Integer)
+    marketCap = db.Column(db.Integer)
 
+    def __init__(self, name, industry, sot, mc):
+        self.companyName = name
+        self.industry = industry
+        self.sharesOutstanding = sot
+        self.marketCap = mc
 
-# Run Server
+# Company Schema
+class CompanySchema(marsh.Schema):
+        class Meta:
+            fields = ('companyName', 'industry', 'sharesOustanding', 'marketCap')
+
+# Init the Schema
+company_schema = CompanySchema()
+companies_schema = CompanySchema(many=True)
+
+# Add a Company to the database
+@app.route('/company', methods=['POST'])
+def addCompany():
+    companyName = request.json['companyName']
+    industry = request.json['industry']
+    sharesOustanding = request.json['sharesOutstanding']
+    marketCap = request.json['marketCap']
+
+    newCompany = Company(companyName, industry, sharesOustanding, marketCap)
+
+    db.session.add(newCompany)
+    db.session.commit()
+    return company_schema.jsonify(newCompany)
+
+# Get a single Company
+@app.route('/company/<companyName>', methods=['GET'])
+def getCompany(companyName):
+    company = Company.query.get(companyName)
+    return company_schema.jsonify(company)
+
+# Update a company
+@app.route('/company/<companyName>', methods=['PUT'])
+def updateCompany(companyName):
+    company = Company.query.get(companyName)
+
+    companyName = request.json['companyName']
+    industry = request.json['industry']
+    sharesOustanding = request.json['sharesOutstanding']
+    marketCap = request.json['marketCap']
+
+    company.companyName = companyName
+    company.industry = industry
+    company.sharesOustanding = sharesOustanding
+    company.marketCap = marketCap
+
+    db.session.commit()
+    return company_schema.jsonify(company)
+
+# Delete company from the database
+@app.route('/company/<companyName>', methods=['DELETE'])
+def deleteCompany(companyName):
+    company = Company.query.get(companyName)
+    db.session.delete(company)
+    db.session.commit()
+    return company_schema.jsonify(company)
+
+# Run server
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
